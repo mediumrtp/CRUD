b5313950951175deaafac93b2f88a71b7bc232c4
Igor Boris Mitrevski <mediumrtp@gmail.com>
2022-11-15 11:36:53 +0100

    initiall commit

---
+++ .history/index_20221114221000.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const router = require('./routes/emp')
+const Emp = require('./models/emp')
+
+var app = express()
+app.use('/', router)
+
+//MongoDB connection
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+mongoose.connection.once('open', function() {
+    console.log('Database connected Successfully');
+}).on('error', function(err) {
+    console.log('Error', err);
+})
+
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/style.css');
+})
+
+
+//Server
+app.listen(8000, function() {
+    console.log('Server is Up')
+})
